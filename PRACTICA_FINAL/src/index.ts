18963c08a6d98947a7a3a4845894def0a1ff5846
Alvaro Fuentes Valverde <alvaro.fuentes@babel.es>
2020-03-04 12:56:26 +0100

    JS maqeutacion hecha

--- PRACTICA_FINAL/src/index.ts
+++ PRACTICA_FINAL/src/index.ts
@@ -1,11 +1,68 @@
-import * as _ from 'lodash';
+const input= document.querySelector('input');
+const TaskList=document.querySelector('#taskList')
+var ArrayTasks=["Hacer comida", "Ver tele"];
 
-  function component() {
-    const element = document.createElement('div');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    return element;
+
+
+
+
+
+//functions
+
+
+function listTaskElements(ArrayTasks: string[]){
+
+  for(let i=0; i<ArrayTasks.length; i++){
+
+    const task=document.createElement('li');
+
+    task.textContent=ArrayTasks[i];
+
+    TaskList.appendChild(task);
+
+
+
   }
 
-  document.body.appendChild(component());
+
+
+}
+
+function addTask(nameTask: string){
+
+  ArrayTasks.push(nameTask);
+  listTaskElements(ArrayTasks);
+
+}
+
+
+
+
+
+
+//events
+input.onkeyup=(e)=>{
+
+
+if(e.key==='Enter'){
+
+  if (input.value) {
+
+  addTask(input.value);
+
+
+input.focus();
+input.select();
+
+}
+
+}
+
+
+
+}
+
+
+
--- PRACTICA_FINAL/webpack.config.js
+++ PRACTICA_FINAL/webpack.config.js
@@ -3,43 +3,31 @@ const CopyPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.ts',
-    devtool: 'inline-source-map',
-    module: {
-
-        plugins: [
-
-            new CopyPlugin([
-                { from: './src/index.html', to: 'dist' },
-
-            ]),
-
-            new CleanWebpackPlugin({
-
-               
-
-                cleanStaleWebpackAssets: false,
-
-            
-            })
-
-        ],
-
-
-
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false
+    }),
+    new CopyPlugin([
+      { from: 'src/index.html', to: 'index.html' },
+      { from: 'src/styles.css', to: 'styles.css' },
+    ]),
+  ],
 };
